845d6c565b6be7a4769b7f91632e92abbe6db4e6
atulrane03 <86475310+atulrane03@users.noreply.github.com>
2022-11-17 18:25:12 +0530

    Add files via upload

---
+++ assign3.sol
@@ -0,0 +1,18 @@
+//SPDX-License-Identifier:MIT
+pragma solidity 0.4.25;
+
+contract Bank{
+    int bal;
+    constructor() public{
+        bal=1;
+    }
+    function depositMoney(int amt)public{
+        bal=bal+amt;
+    }
+    function withdrawMoney(int amt) public{
+        bal=bal-amt;
+    }
+    function showBalance() view public returns(int){
+        return bal;
+    }
+}
